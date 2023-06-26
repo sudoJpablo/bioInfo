a1074e3fb6ae581e39a5894bb7cfc64ce18db9cf
ClaudioPM22 <cpinillam2340@gmail.com>
2023-06-26 00:05:19 -0400

    Solo falta la transcripcion a proteina

--- tarea2/ORF_transcript.py
+++ tarea2/ORF_transcript.py
@@ -3,28 +3,64 @@
 import sys
 import re
 
-def funcion(cadena, ini, fin):
-    i = re.search(f'{ini}', cadena)
-    f = re.search(f'{fin}', cadena)#Este no me sirve, tengo que despues de encontrar el primero
-    #buscar de tres en tres las coicidencias de los stop
-    print(f'{i.group()},{i.start()} {f.group()},{f.start()}')
-    return cadena[i.start():f.end()]
+def funcion(cadena, fin):
+    condicion= False #Cuando sea false no se agragaran elementos a orf
+    cont=3 #Para ir de 3 en 3 en la cadena
+    orf = '' #Este se encarga de reunir los codones que forman el ORF
+    orfs = [] #Aqui se guardan los ORFs obtenidos en el marco de lectura
+    while(cont <= len(cadena)):
+        codon = cadena[cont-3:cont]
+        if(codon== 'ATG' or codon== 'AUG'):
+            condicion = True
+        if(condicion):
+            orf+= codon
+        if(fin.find(codon)!=-1 and condicion):
+            condicion=False
+            orfs.append(orf)
+            orf=''
+        cont+=3
+    return orfs
+
+def obtener_hebra_negativa(secuencia):
+    complemento = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+    hebra_negativa = ''.join(complemento[base] for base in reversed(secuencia))
+    return hebra_negativa
+
+def imprime(orf,cadena,signo):
+    for i in orf:
+        print(f'{i} {cadena.find(i)} {len(i)} {signo} {transcripcion(i)}')
+
+def transcripcion(orf):
+    return 'proteina'
 
 def main():
     if len(sys.argv) != 2:
         print('Debe ejecutarse como ./ORF_transcript.py <Secuencia ADN>')
         sys.exit()
 
-    secuencia = sys.argv[1]
-    start = 'AUG|ATG'
-    stop = 'UAA|UAG|UGA|TAA|TAG|TGA'
+    secuenciaPositiva = sys.argv[1]
+    stop = '(UAA|UAG|UGA|TAA|TAG|TGA)'
+    secuenciaNegativa = obtener_hebra_negativa(secuenciaPositiva)
 
-    # marcos de lectura
-    for i in range(2):
-        print(f'\nMarco de lectura {i}:')
-        orf = funcion(secuencia[i:], start, stop)
-        print(orf)
+    # marcos de lectura hebra positiva
+    for i in range(3):
+        orf = funcion(secuenciaPositiva[i:], stop)
+        if(orf!=[]):imprime(orf,secuenciaPositiva,'+')
+    # marcos de lectura hebra negativa
+    for i in range(3):
+        orf = funcion(secuenciaNegativa[i:], stop)
+        if(orf!=[]):imprime(orf,secuenciaNegativa,'-')
+        
+        
 
 if __name__ == "__main__":
     main()
 
+"""
+secuencia
+AGATGCCTATGGATGTTAATGATTGAGTGATATAACG
+resultados
+ATGTTAATGATTGAGTGA 11   18  + MLMIE
+ATGCCTATGGATGTTAATGATTGA    2   24  +   MPMDVND
+
+"""
