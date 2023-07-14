25853815da272f3e490fe2274974b73ccd0be5a0
ClaudioPM22 <cpinillam2340@gmail.com>
2023-07-14 14:15:17 -0400

    Codigo sin contar exones pero completo

--- tarea4/Blastea_e_informa.py
+++ tarea4/Blastea_e_informa.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from Bio.Seq import Seq
 from Bio.Blast import NCBIWWW, NCBIXML
+from Bio import Entrez
+from Bio import SeqIO
 import sys
 
 def blasteando(seq):
@@ -10,16 +12,37 @@ def blasteando(seq):
     with open("resultado_blast.xml", "w") as out_handle:
         out_handle.write(resultados.read())
     resultados.close()
+    print('***archivo almacenado***')
     return
 
 def informacion():
     print("Obteniendo informacion del gen...")
     resultados = open("resultado_blast.xml")
-    blast_record= NCBIXML.read(resultados)
-    alineamiento = blast_record.alignments[0]
-    
+    blast_record= NCBIXML.read(resultados)      #gi|71143213|gb|AC165171.2|
+    alineamiento = blast_record.alignments[0]   #0123456789
+    partes = alineamiento.hit_id.split('|')
+    id = partes[3].split('.')
     resultados.close()
-    return alineamiento.hit_id
+    return id[0]
+    
+
+def accediendoNCBI(id_gen):
+    Entrez.email = "micorreo@uach.cl"
+
+    with Entrez.efetch(db="nucleotide", rettype="gb", retmode="text", id=id_gen) as handle:
+        seq_record = SeqIO.read(handle, "gb")
+
+        print(f"Especie: {seq_record.annotations['organism']}")
+        print(f"Indentificador del gen: {seq_record.annotations['accessions'][0]}")
+        print(f"Ubicacion: {seq_record.features[0].location}")
+        
+        c=0
+        for i in seq_record.features:
+            if(i.type == 'exon'):
+                c+=1
+        print(f"Exones en el gen: {c}")
+    
+    return
 
 def main():
     if(len(sys.argv) != 2):
@@ -29,25 +52,7 @@ def main():
     my_seq = Seq(sys.argv[1])
     #blasteando(my_seq)
     gen_id = informacion()
-    print(gen_id)
-    
+    accediendoNCBI(gen_id)
 
-print('***archivo almacenado***')
 
 main()
-"""
-Cree un programa ejecutable desde la línea de comandos,
-que al ser invocado reciba como argumento una secuencia query. 
-Esta secuencia deberá ser BLASTeada online. 
-De los resultados obtenidos (correspondientes a alineamientos), 
-el programa deberá considerar el primer HSP del primer Hit y extraer 
-el identificador del gen en el cual se encuentra, 
-para posteriormente descargar información sobre este gen y 
-mostrar la siguiente información:
-
-- Especie a la que pertenece
-- Gen al cual pertenece, indicando:
-    - cromosoma en el que se encuentra
-    - Ubicación y hebra (+/-)
-    - número de exones que posee el gen
-"""
